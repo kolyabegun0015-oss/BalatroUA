5bab591fc3d2c9636cf37d25f6ba9b4d64d2a91a
kolyabegun0015-oss <kolyabegun0015@gmail.com>
2025-08-26 06:41:46 +0200

    Add files via upload

---
+++ localization/uk.lua
@@ -0,0 +1,20 @@
+return {
+  misc = {
+    labels = {
+      play = "Грати",
+      options = "Налаштування",
+      quit = "Вийти"
+    }
+  },
+  v_dictionary = {
+    remaining = {"Залишилось #1# спроб", "Залишилась #1# спроба"}
+  },
+  descriptions = {
+    Joker = {
+      j_stone = {
+        name = "Кам’яний Джокер",
+        text = {"Дає {X:mult}x#1#{} до множника."}
+      }
+    }
+  }
+}
